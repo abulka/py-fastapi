42991054e5588ca5b0a6fc2577fc991e8395a2c6
Andy <abulka@gmail.com>
2026-03-10 12:50:20 +1100

    emoji

--- app/routers/info.py
+++ app/routers/info.py
@@ -5,11 +5,9 @@ import time
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
-
 @router.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "FastAPI Random Info Test"})
-
 @router.get("/random-info")
 async def get_random_info():
     info_list = [
@@ -18,6 +16,7 @@ async def get_random_info():
         f"Random number: {random.randint(1, 1000)}",
         "This data was fetched asynchronously.",
         "Fedora Silverblue + Podman detected!",
+        "Andy was here. 🤓😁😆😆😆😆 🔥",
         "Containers make development reproducible."
     ]
     return {"data": random.choice(info_list), "success": True}
